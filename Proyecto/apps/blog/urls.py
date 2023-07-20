b939c2899251d4ffbe8c756f72a7211ce7abcfe7
TGerzenstein <tatigerzens@gmail.com>
2023-07-20 15:40:05 -0300

    Actualizaciones en blog

--- Proyecto/apps/blog/urls.py
+++ Proyecto/apps/blog/urls.py
@@ -11,7 +11,7 @@ app_name= 'apps.blog'
 urlpatterns = [
     path('agregar_categoria/', AgregarCategoria.as_view(),name='agregar_categoria'),
     path('agregar_post', AgregarPost.as_view(), name='agregar_post'),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT),
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
