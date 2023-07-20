b2832bed881c89cbc35cbd3a707d09e22c688e11
TGerzenstein <tatigerzens@gmail.com>
2023-07-20 18:40:32 -0300

    Agregué modificaciones en los path

--- Proyecto/apps/usuarios/urls.py
+++ Proyecto/apps/usuarios/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import RegistrarUsuario
 
-
+app_name = 'apps.usuarios'
 
 urlpatterns = [
     path("registrar/", RegistrarUsuario.as_view(), name='registrar'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
