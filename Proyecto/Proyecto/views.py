73c91c9378b4c976d6637ce4ef3f349c6045308f
TGerzenstein <tatigerzens@gmail.com>
2023-07-21 17:19:09 -0300

    Generé las funciones para views nosotros y contacto

--- Proyecto/Proyecto/views.py
+++ Proyecto/Proyecto/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html') 
 
+def contacto(request):
+    return render(request, 'contacto.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
