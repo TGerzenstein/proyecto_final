577a6a209cac36f3e764437a6fcab29293c7805c
TGerzenstein <tatigerzens@gmail.com>
2023-07-25 08:58:53 -0300

    Updated

--- Proyecto/apps/blog/views.py
+++ Proyecto/apps/blog/views.py
@@ -3,19 +3,19 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Categorias, Post
 from django.views.generic import ListView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class AgregarCategoria(CreateView):
+class AgregarCategoria(LoginRequiredMixin, CreateView):
     model = Categorias
     fields = ['nombre']
     template_name = 'blog/agregar_categoria.html'
     success_url = reverse_lazy('index')
 
 
-class AgregarPost(CreateView):
+class AgregarPost(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['category','titulo','texto','imagen']
     template_name = 'blog/agregar_post.html'
