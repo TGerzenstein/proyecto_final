8f35ae9cc85f7edd62e54a9185f65b0091cdaf52
TGerzenstein <tatigerzens@gmail.com>
2023-07-25 12:52:40 -0300

    Actualicé paths

--- Proyecto/apps/blog/urls.py
+++ Proyecto/apps/blog/urls.py
@@ -10,7 +10,7 @@ app_name= 'apps.blog'
 
 urlpatterns = [
     path('agregar_categoria/', AgregarCategoria.as_view(),name='agregar_categoria'),
-    path('agregar_post', AgregarPost.as_view(), name='agregar_post'),
+    path('agregar_post/', AgregarPost.as_view(), name='agregar_post'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
