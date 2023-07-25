ada3652fd1ee907761304e191e7f892223ade8ff
TGerzenstein <tatigerzens@gmail.com>
2023-07-25 20:15:19 -0300

    Updated css, templates, urls

--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -4,18 +4,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from .views import index, contacto, nosotros
+from .views import *
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index, name='index'),
+    path('', index, name='index'),
     path('contacto/', contacto, name='contacto'),
     path('nosotros/', nosotros, name='nosotros'),
     path('blog/', include('apps.blog.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
-    path('Comentarios/', include('apps.Comentarios.urls')),
+    path('comentarios/', include('apps.Comentarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
